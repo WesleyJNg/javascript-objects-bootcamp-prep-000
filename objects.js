f18fb7c9a0c06bac0b1449ae2c4c5427236844d9
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-17 21:29:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {name: 'song'}
 
 function updatePlaylist(playlistObj,artistName,songTitle) {
   playlistObj.artistName = songTitle;
-  return playstObj;
+  return playlistObj;
 }
 
 function removeFromPlaylist(playlistObj,artistName) {
   delete playlistObj.artistName;
-  return playstObj;
+  return playlistObj;
 }
